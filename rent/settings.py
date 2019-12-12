1b9ad581eadfe395e37834ca82325dbf1d8b32a6
pratheebapalanisami <pnalligounderpalan@unomaha.edu>
2019-12-12 12:20:18 -0600

    Removed api changes

--- rent/settings.py
+++ rent/settings.py
@@ -43,11 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rentapp',
     'crispy_forms',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
